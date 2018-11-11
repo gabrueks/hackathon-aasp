16ccd025c9e48b2e94dbf44e09dfa0bc56029f46
n210163 <gabrielbsouza@santander.com.br>
2018-11-11 11:46:45 -0200

    changes

--- src/Components/adv-me/App.js
+++ src/Components/adv-me/App.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 
-// import PieChart from './Chart';
 import { Header, Footer } from '../Layouts';
 
 export default class extends Component {
@@ -9,7 +8,7 @@ export default class extends Component {
         return (
             <Fragment>
                 <Header/>
-                    <img width='100%' src='https://scontent-gru2-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/45874825_2121973708053118_8444883990111846400_n.png?_nc_cat=107&_nc_ht=scontent-gru2-1.xx&oh=7fa3018aeb9967166c397772d69e1714&oe=5C704B51' alt="data"></img>
+                    <img width='100%' src='https://scontent-gru2-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/46053355_302940987208271_1899047861832646656_n.png?_nc_cat=102&_nc_ht=scontent-gru2-1.xx&oh=7fd0cd3b47646636c21774186c3ef18d&oe=5C874D5B' alt="data"></img>
                  <Footer
                  />
              </Fragment>
